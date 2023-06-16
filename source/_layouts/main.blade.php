345b479cfe3f3a1bf35f9c12d2cd39959fdf1fad
fkcjf <j.fibich@fkc-online.com>
2023-06-16 15:40:51 +0200

    Reorganize layout

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -26,23 +26,10 @@
     </head>
 
     <body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
-        <header class="flex items-center shadow bg-white border-b h-24 py-4" role="banner">
-            <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
-                <div class="flex items-center">
-                    <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
-                        <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
-
-                        <h1 class="text-lg md:text-2xl text-blue-800 font-semibold hover:text-blue-600 my-0">{{ $page->siteName }}</h1>
-                    </a>
-                </div>
-
-                <div id="vue-search" class="flex flex-1 justify-end items-center">
-                    <search></search>
-
-                    @include('_nav.menu')
-
-                    @include('_nav.menu-toggle')
-                </div>
+        <header>
+            @include('_layouts.partials.navbar')
+            <div>
+                @yield('header')
             </div>
         </header>
 
@@ -52,18 +39,7 @@
             @yield('body')
         </main>
 
-        <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
-            <ul class="flex flex-col md:flex-row justify-center list-none">
-                <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                </li>
-
-                <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
-                </li>
-            </ul>
-        </footer>
+        @include('_layouts.partials.footer')
 
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
---
+++ source/_layouts/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
+    <ul class="flex flex-col md:flex-row justify-center list-none">
+        <li class="md:mr-2">
+            TODO: add company info.
+        </li>
+
+        <li>
+            Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
+            and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+        </li>
+    </ul>
+</footer>
